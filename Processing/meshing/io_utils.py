f47f0deb188ae2156179142e9bd559dc6211983e
Joel Salzman <joelkevlessalzman@gmail.com>
2024-10-21 23:07:08 -0700

    pushing meshes

--- Processing/meshing/io_utils.py
+++ Processing/meshing/io_utils.py
@@ -6,6 +6,7 @@ Created on Mon Jul  1 12:38:49 2024
 """
 import numpy as np
 import open3d as o3d
+import os
 
 
 def o3d_mesh_to_blender(o3d_mesh, name):
@@ -38,9 +39,10 @@ def write_ply(points, colors, file_path):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
     
-    if colors is not None:
+    if colors is not None and len(colors):
         if np.max(colors) > 1:
             colors = colors / 255.0
         pcd.colors = o3d.utility.Vector3dVector(colors)
     
     o3d.io.write_point_cloud(file_path, pcd, write_ascii=False)
+
