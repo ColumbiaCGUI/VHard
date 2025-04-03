e8537794f284884c9796bf748e5f3ea258d82cab
Amanda Li <ajl2315@columbia.edu>
2025-04-03 18:36:49 -0400

    Made minimap work, need to adjust movement distance and UI features

--- Assets/MinimapClickHandler.cs
+++ Assets/MinimapClickHandler.cs
@@ -4,11 +4,12 @@ using UnityEngine.EventSystems;
 public class MinimapClickHandler : MonoBehaviour, IPointerClickHandler
 {
     public Camera minimapCamera;  // Assign the Minimap Camera in the Inspector
-    public Camera mainCamera;     // Assign the Main Camera in the Inspector
+    public Transform cameraOffset;     // Assign the Main Camera in the Inspector
     public Transform climbingWall; // Assign the Climbing Wall in the Inspector
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        Debug.Log("Minimap clicked!");
         // Get the Minimap RectTransform
         RectTransform minimapRect = GetComponent<RectTransform>();
 
@@ -23,7 +24,7 @@ public class MinimapClickHandler : MonoBehaviour, IPointerClickHandler
         Vector3 worldPosition = ConvertMinimapToWorld(normalizedPoint);
 
         // Move the main camera to the new position
-        mainCamera.transform.position = new Vector3(worldPosition.x, worldPosition.y, mainCamera.transform.position.z);
+        cameraOffset.transform.position = new Vector3(worldPosition.x, worldPosition.y, cameraOffset.transform.position.z);
     }
 
     private Vector3 ConvertMinimapToWorld(Vector2 normalizedPoint)
@@ -33,6 +34,6 @@ public class MinimapClickHandler : MonoBehaviour, IPointerClickHandler
         float worldX = Mathf.Lerp(wallBounds.min.x, wallBounds.max.x, normalizedPoint.x);
         float worldY = Mathf.Lerp(wallBounds.min.y, wallBounds.max.y, normalizedPoint.y);
 
-        return new Vector3(worldX, worldY, mainCamera.transform.position.z);
+        return new Vector3(worldX, worldY, cameraOffset.transform.position.z);
     }
 }
